9d506b97a2bf64b75d3f6a101dc811eaf4754e78
AbeerDhoke <abeerdhoke@gmail.com>
2026-09-15 23:59:24 +0530

    made some changes

---
+++ 06 - strings/strings.js
@@ -0,0 +1,23 @@
+const name = "Abeer";
+const repoCount = 50;
+console.log(name + repoCount);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String('AbeerDhoke')
+console.log(gameName.__proto__);
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('e'));
+const newString = gameName.substring(0,5);
+console.log(newString);
+const anotherString = gameName.slice(-9, 3);
+console.log(anotherString);
+const newStringOne = "  Abeer  ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+console.log(gameName.replace('A', 'Ka'));
+
+
+
+
