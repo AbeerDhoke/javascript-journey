4ef8b7eca12ad9275a3cb15b4803dc4413a0e883
AbeerDhoke <abeerdhoke@gmail.com>
2026-09-22 00:30:49 +0530

    made some changes in folder names

--- 07-num-and-maths/nums.js
+++ 07-num-and-maths/nums.js
@@ -2,7 +2,6 @@ const balance = new Number(100);
 console.log(balance);
 console.log(balance.toString().length);
 console.log(balance.toFixed(2));
-
 const value = 56.824;
 console.log(value.toPrecision());
 const data = 1000000;
--- Topics/07-num-and-maths/maths.js
+++
@@ -1,7 +0,0 @@
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.abs(-4));
-console.log(Math.round(7.5));
-console.log(Math.random());
-
-
